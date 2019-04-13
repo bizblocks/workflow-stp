b82392e407c9787bbf95ea3b559ee8c3eff1bbaa
gecrepo <gecrepo@gmail.com>
2019-04-14 01:17:34 +0400

    WorkflowDetails annotation to support external metadata of workflow process

---
+++ modules/global/src/com/groupstp/workflowstp/annotation/WorkflowDetails.java
@@ -0,0 +1,30 @@
+package com.groupstp.workflowstp.annotation;
+
+import com.haulmont.cuba.core.entity.annotation.MetaAnnotation;
+import org.apache.commons.lang.StringUtils;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Additional meta data of workflow behaviour for each workflow entity
+ *
+ * @author adiatullin
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@MetaAnnotation
+public @interface WorkflowDetails {
+
+    /**
+     * @return expected extending browser screen id
+     */
+    String browseId() default StringUtils.EMPTY;
+
+    /**
+     * @return expected extending editor screen id
+     */
+    String editorId() default StringUtils.EMPTY;
+}
