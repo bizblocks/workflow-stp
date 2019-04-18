94a80113ccabac2eebf7206a2ae9d2d48d0fc307
adiatullin <adiatullin@haulmont.com>
2019-04-18 16:51:01 +0400

    Rest controller structure

--- modules/web/src/com/groupstp/workflowstp/rest/controller/WorkflowRestController.java
+++ modules/web/src/com/groupstp/workflowstp/rest/controller/WorkflowRestController.java
@@ -1,8 +1,15 @@
 package com.groupstp.workflowstp.rest.controller;
 
+import com.haulmont.cuba.core.app.importexport.EntityImportExportService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.inject.Inject;
 
 /**
  * Workflow REST controller
@@ -12,4 +19,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("wfstp_WorkflowRestController")
 @RequestMapping(value = "/workflow", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class WorkflowRestController {
+
+    @Inject
+    private EntityImportExportService entityImportExportService;
+
+
+    public void getWorkflows() {
+        //all active visible to user workflows with stages and permission
+
+    }
+
+    public void start(@RequestParam String id, @RequestParam String entityName) {
+
+    }
+
+    public void start(@RequestParam String id, @RequestParam String entityName, @RequestParam String workflowId) {
+
+    }
+
+    public boolean isProcessing(@RequestParam String id, @RequestParam String entityName) {
+        return false;
+    }
+
+    public boolean isPerformable(String workflowId, String stageId, String actionId) {
+        return false;
+    }
+
+    public void perform(String workflowId, String stageId, String actionId) {
+
+    }
 }
